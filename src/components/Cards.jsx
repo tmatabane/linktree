a125b5a59c9feb47a877dbe177fafd3a16b3ae72
Thato M <thatomatabane@hotmail.com>
2022-06-24 17:15:32 +0200

    Full width flex buttons

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 function Cards(props) {
     return(
-        < div className="container">
         
         <a href={props.url}>
-        <div className="card">
+        <div className="container">
             <h1>{props.name}.
             <i class={props.iconn}></i>
             </h1>
         </div>
         </a>
-        </div>
     );
 }
 
