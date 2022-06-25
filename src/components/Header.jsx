390adaeb07ee9da57f9ab3e006ed6ed11d58b330
Thato M <thatomatabane@hotmail.com>
2022-06-25 07:00:48 +0200

    current time displays above header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "./styles.css"
+
+const currentTime = new Date();
+
+const hrs = currentTime.getHours();
+
+const min = currentTime.getMinutes();
+
+const time = hrs + ":" +  min + "";
+
 function Header() {
-    return(<div><p>Time</p><header><h1 className="heading">Morning an Aspring Software Engineer's Linktree</h1></header></div>);
+    return(<div><h2>{time} - SAST</h2><header><h1 className="heading">Morning an Aspring Software Engineer's Linktree</h1></header></div>);
 }
 
 export default Header;
